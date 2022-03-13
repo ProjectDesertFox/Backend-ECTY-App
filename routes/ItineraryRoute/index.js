36b55ba2f2a830ef5cc0f0550c28b374154fcdfd
bayuindr <bayuindr2207@gmail.com>
2022-03-13 12:52:41 +0700

    up

--- routes/ItineraryRoute/index.js
+++ routes/ItineraryRoute/index.js
@@ -6,7 +6,7 @@ console.log('router masuk');
 
 Itineraries.get('/', ControllerItinerary.fetchAllItinerary)
 Itineraries.post('/', ControllerItinerary.addItinerary)
-Itineraries.get('/my-list', ControllerItinerary.fetchAllItineraryMyList)
+// Itineraries.get('/my-list', ControllerItinerary.fetchAllItineraryMyList)
 Itineraries.get('/:id', ControllerItinerary.fetchOne)
 Itineraries.put('/:id', ControllerItinerary.update)
 Itineraries.delete('/:id', ControllerItinerary.delete)
